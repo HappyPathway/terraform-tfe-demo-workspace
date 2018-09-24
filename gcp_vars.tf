0b7ac6a5d250678fc66af2e49a81e94d852c6273
David Arnold <dja+djaboxx@sonic.net>
2018-09-24 12:43:10 -0700

    Update gcp_vars.tf

--- gcp_vars.tf
+++ gcp_vars.tf
@@ -17,7 +17,7 @@ resource "tfe_variable" "gcp_credentials" {
     key = "GOOGLE_CREDENTIALS"
     value = "${data.vault_generic_secret.gcp.data["json"]}"
     category = "env"
-    workspace_id = "${module.workspace.workspace_id}"
+    workspace_id = "${tfe_workspace.ws.id}"
     sensitive = true
 }
 
@@ -27,6 +27,6 @@ resource "tfe_variable" "GOOGLE_PROJECT" {
     key = "GOOGLE_PROJECT"
     value = "${data.vault_generic_secret.gcp.data["project_id"]}"
     category = "env"
-    workspace_id = "${module.workspace.workspace_id}"
+    workspace_id = "${tfe_workspace.ws.id}"
     sensitive = true
 }
